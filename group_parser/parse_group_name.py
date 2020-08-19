3bf6232de137c969b884c409da1ec0be8d494f4a
kiri11-mi1 <mil.kirillka@mail.ru>
2020-08-19 21:59:38 +0700

    Реализовал парсер имён групп по id

---
+++ group_parser/parse_group_name.py
@@ -0,0 +1,43 @@
+import requests
+from bs4 import BeautifulSoup
+import re
+import json
+
+groups = {}
+
+
+def is_group(soup):
+    element = soup.find('h3', {'class': 'text-center'})
+    if element is not None:
+        return element.text
+    return False
+
+def parse_name_group(string):
+    string = re.sub(r"\s", "", string)
+    string = ''.join(list(string)[1:])
+    name_group = string[: string.find("\"")]
+    return name_group
+
+
+def get_name_groups_by_id():
+    for id_group in range(5047, 5090):
+        # Получение страницы
+        response = requests.get(
+            f'https://timetable.pallada.sibsau.ru/timetable/group/{id_group}'
+        )
+
+        html = response.text
+        soup = BeautifulSoup(html, 'html.parser')
+        group = is_group(soup)
+
+        # Проверка на существование группы
+        if group is not False:
+            groups.update({ parse_name_group(group) : id_group })
+
+    return groups
+
+
+def save_data_in_json(data):
+    f = open('name_groups.json', 'w', encoding="utf-8")
+    json.dump(data, f, ensure_ascii=False, indent=4)
+
