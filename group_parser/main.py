dab1a6f0cf2a5970e53821ec0027d6e2c5ea0945
kiri11-mi1 <mil.kirillka@mail.ru>
2020-08-20 04:43:43 +0700

    Антон, выручайgit add main.pygit add main.py

--- group_parser/main.py
+++ group_parser/main.py
@@ -1,17 +1,15 @@
 from group_parser import GroupParser
-import threading
 import time
 
 begin = time.time()
-
 gp = GroupParser()
 
 for i in range(0, 6000):
-    x = threading.Thread(target=gp.get_name_groups_by_id, args=(i, ))
-    x.start()
+    gp.get_name_groups_by_id(i)
 
 gp.save_data_in_json()
 
 end = time.time()
 
 print(f'Time: {end-begin}')
+
