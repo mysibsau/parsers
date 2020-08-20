8803b3e02a993b5f36748c003b13d2a6c404ce5d
w0rng <antonabramov2000@gmail.com>
2020-08-20 23:50:42 +0700

    Мини рефакторнг

--- group_parser/group_parser.py
+++ group_parser/group_parser.py
@@ -1,32 +1,26 @@
 import requests
 from bs4 import BeautifulSoup
-from multiprocessing import Pool
+from multiprocessing.pool import ThreadPool
 import re
 import json
 
 
 class GroupParser:
     def __init__(self):
-        self.groups = {}
+        self.groups = []
 
 
-    def get_name_group(self, soup):
+    def get_name_group(self, soup: BeautifulSoup):
         element = soup.find('h3', {'class': 'text-center'})
-        if element is not None:
+        if element:
             return element.text
-        return False
 
 
-    def parse_name_group(self, string):
-        string = re.sub(r"\s", "", string)
-        string = ''.join(list(string)[1:])
-        name_group = string[: string.find("\"")]
-        return name_group
+    def parse_name_group(self, string: str) -> str:
+        return string.split('"')[1]
 
 
-    def get_group_by_id(self, id_group):
-        '''Возвращает словарь из одного элемента типа "название_группы": "id_группы"'''
-
+    def get_group_by_id(self, id_group: int):
         html = requests.get(
             f'https://timetable.pallada.sibsau.ru/timetable/group/{id_group}'
         ).text
@@ -35,30 +29,25 @@ class GroupParser:
         group = self.get_name_group(soup)
 
         # Проверка на существование группы
-        if group is not False:
-            return { self.parse_name_group(group) : id_group }
+        if group:
+            return [id_group, self.parse_name_group(group)]
 
 
     def set_groups(self):
         '''Формирует словарь типа "название_группы": "id_группы"'''
 
-        # Диапазон id групп
-        id_list = list( range(6000) )
-
-        # Устанавливаем пул из 100 процессов - это оптимальное на моём комплюхтере
-        pool = Pool(processes=100)
+        id_list = list(range(6000))
+        pool = ThreadPool(16)
         
         # Формируем словарь
         for group in pool.map(self.get_group_by_id, id_list):
-            if group is not None:
-                self.groups.update( group )
+            if group:
+                self.groups.append(group)
         
         pool.close()
 
 
     def save_data_in_json(self):
-        '''Сохраняет группы в json'''
-
-        f = open('name_groups.json', 'w', encoding="utf-8")
-        json.dump(self.groups, f, ensure_ascii=False, indent=4)
-        f.close()
+        with open('name_groups.csv', 'w', encoding="utf-8") as f:
+            for i in self.groups:
+                f.write(f'{i[0]};{i[1]}')
