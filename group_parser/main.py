3be45b8a411f08023d34f0c14085eb884d7dda22
kiri11-mi1 <mil.kirillka@mail.ru>
2020-08-20 17:52:36 +0700

    Тут можно поиграться с парсером

--- group_parser/main.py
+++ group_parser/main.py
@@ -1,14 +1,12 @@
 from group_parser import GroupParser
 import time
 
+
 begin = time.time()
+
 gp = GroupParser()
-
-for i in range(0, 6000):
-    gp.get_name_groups_by_id(i)
-
+gp.set_groups()
 gp.save_data_in_json()
 
-end = time.time()
-
-print(f'Time: {end-begin}')
+final_time = time.time() - begin
+print(f'Time: {final_time // 60} min({final_time} sec)')
