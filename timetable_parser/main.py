0df6c06a45512fd43c789a26c3178872af4b909b
w0rng <antonabramov2000@gmail.com>
2020-08-22 14:04:06 +0700

    Убрал лишие данные

--- timetable_parser/main.py
+++ timetable_parser/main.py
@@ -2,19 +2,14 @@ from parser import Parser
 
 
 def main():
-    p = Parser()
 
-    # БПИ
-    id_group_bpi = 5047
+    time_table = Parser().get_timetable(5047)
+    print(time_table)
 
-    # РС
-    id_group_rs = 5080
-    time_table = p.get_timetable_for_group(id_group_bpi)
-
-    for week in time_table['timetable'].keys():
-        for day in time_table['timetable'][week].keys():
+    for week in time_table:
+        for day in time_table[week]:
             print(f'{day.capitalize()}:')
-            for subject in time_table['timetable'][week][day]:
+            for subject in time_table[week][day]:
                 for key, value in subject.items():
                     print(f'\t{key}: {value}')
                 print()
