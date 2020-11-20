646acaea54f37bbe6be6d00cab1d841179f64990
Kirill Mil <kirill.mil.0310@mail.ru>
2020-11-20 22:34:51 +0700

    главный файл

---
+++ consultation_parser/main.py
@@ -0,0 +1,21 @@
+from parser import ParseConsultation
+
+
+def main():
+
+    consultation = ParseConsultation().get_consultation(1397)
+
+    if consultation:
+
+        for week in consultation:
+            print(week)
+            for day in consultation[week]:
+                for key, value in day.items():
+                    print(key, ':', value)
+                print()
+            print()
+            print()
+
+
+if __name__ == '__main__':
+    main()
