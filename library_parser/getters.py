b05b42842fd6013e874f0c1d345a953036793ea2
kiri11_mi1 <kirill.mil.0310@mail.ru>
2021-03-17 00:29:22 +0700

    метод для получения таблицы книгохранения

--- library_parser/getters.py
+++ library_parser/getters.py
@@ -2,23 +2,23 @@ import requests
 from random import randint
 
 
-SEARCH_BROADCAST_URL = 'http://biblioteka.sibsau.ru/jirbis2/components/com_irbis/ajax_provider.php'
+BASE_URL = 'http://biblioteka.sibsau.ru'
 
 
-def get_random_req_id_client():
+def get_random_req_id_client() -> int:
     return randint(1, 900000)
 
 
 def get_books_from_library(key_words: str = 'программирование', physical: bool = True) -> str:
     if not physical:
         response = requests.get(
-            f'http://biblioteka.sibsau.ru/jirbis2/components/com_irbis/ajax_provider.php?task=set_selected_bases&bl_id_array_selected%5B1%5D=3&bl_id_array_selected%5B11%5D=11&_=1615539096378'
+            f'{BASE_URL}/jirbis2/components/com_irbis/ajax_provider.php?task=set_selected_bases&bl_id_array_selected%5B1%5D=3&bl_id_array_selected%5B11%5D=11&_=1615539096378'
         )
 
     req_id_client = get_random_req_id_client()
 
     requests.post(
-        SEARCH_BROADCAST_URL,
+        BASE_URL + '/jirbis2/components/com_irbis/ajax_provider.php',
         data={
             'fasets': '',
             'req_static': 1,
@@ -29,10 +29,19 @@ def get_books_from_library(key_words: str = 'программирование', 
             "selected_search_flag": 0,
         },
     )
-    SHOW_RESULTS_URL = f'http://biblioteka.sibsau.ru/jirbis2/components/com_irbis/ajax_provider.php?task=show_results&req_id_client={req_id_client}&first_number=1&recs_outputed=0&reqs_outputed=0&last_output_time=0&finish_flag=last&_=1615539096361'
-    response = requests.get(SHOW_RESULTS_URL).json()
+
+    response = requests.get(
+        f'{BASE_URL}/jirbis2/components/com_irbis/ajax_provider.php?task=show_results&req_id_client={req_id_client}&first_number=1&recs_outputed=0&reqs_outputed=0&last_output_time=0&finish_flag=last&_=1615539096361'
+    ).json()
 
     return response['recs']
+
+
+def get_book_holders(url_part: str) -> str:
+    return requests.get(
+        BASE_URL+url_part
+    ).text
+
 
 
 '''def get_books(key_word: str, physical: bool = True) -> list:
@@ -44,5 +53,5 @@ def get_books_from_library(key_words: str = 'программирование', 
 
 
 if __name__ == '__main__':
-    print(get_books_from_library('Программирование', True))
+    print(get_books_from_library(key_words='Программирование', physical=False))
     # print(get_books_from_library('Программирование', False))
