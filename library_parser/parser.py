ced10c10d5b487827c9732db3f1460259067afa8
kiri11_mi1 <kirill.mil.0310@mail.ru>
2021-03-16 17:05:16 +0700

    написал парсер имён и наименований книг

--- library_parser/parser.py
+++ library_parser/parser.py
@@ -8,27 +8,38 @@ def get_book_quantities(root):
 
 
 def get_author_name(root, num):
-    # return soup.find_all('table', {'class': 'record'})
-    # return soup.find_all('tr')
-    
-    return root.select('div.bo_div')[num].text
+    if author := root.cssselect('div.bo_div')[num].cssselect('b')[1].text.strip():
+        return author
 
 
-def get_physical_books(html: str) -> list:
+def get_name_book(root, num):
+    text = ' '.join([item.strip() for item in root.cssselect("div.bo_div")[num].xpath('./text()')]).strip()
+    return text\
+        .split('>> ')[-1]\
+        .split('\xa0\xa0\xa0\xa0')[-1]\
+        .split(':')[0]\
+        .split('/')[0].strip()
+
+
+def get_physical_books(content: str) -> list:
     # временная переменная, потом убрать
     # content = get_books_from_library()
 
     root = html.parse(StringIO(content)).getroot()
     result = []
 
-    for num in range(get_book_quantities):
+    for num in range(get_book_quantities(root)):
         result.append({
             'author': get_author_name(root, num),
+            'name': get_name_book(root, num),
         })
 
     return result
 
 
 if __name__ == '__main__':
-    content = open('test.html').read()
-    print(get_physical_books(content))
+    content = get_books_from_library()
+    for book in get_physical_books(content):
+        for key, value in book.items():
+            print(key, ':', value)
+        print()
