8bc0c6947f33156328f1db56fa304f68e96dfd6d
Kirill Mil <kirill.mil.0310@mail.ru>
2021-03-16 14:30:40 +0700

    add dependices

--- library_parser/parser.py
+++ library_parser/parser.py
@@ -1,33 +1,34 @@
-from bs4 import BeautifulSoup
-import re
+from lxml import html
+from io import StringIO
 from getters import get_books_from_library
 
 
-def get_book_quantities(soup):
-    return len(soup.find_all('table', {'class': 'record'}))
+def get_book_quantities(root):
+    return len(root.cssselect('table.record'))
 
 
-def get_author_name(soup, num):
-    return soup.find_all('div', {'class': 'bo_div'})[num].text
-    #return soup.select('#bo_tab-2145830616 > div.bo_div > b:nth-child(3)')
-
-
-def get_digital_books(html: str) -> list:
-    soup = BeautifulSoup(html, 'html.parser')
+def get_author_name(root, num):
+    # return soup.find_all('table', {'class': 'record'})
+    # return soup.find_all('tr')
+    
+    return root.select('div.bo_div')[num].text
 
 
 def get_physical_books(html: str) -> list:
-    soup = BeautifulSoup(html, 'html.parser')
+    # временная переменная, потом убрать
+    # content = get_books_from_library()
+
+    root = html.parse(StringIO(content)).getroot()
     result = []
 
-    for num in range(get_book_quantities(soup)):
+    for num in range(get_book_quantities):
         result.append({
-            'author': get_author_name(soup, num),
+            'author': get_author_name(root, num),
         })
 
     return result
 
 
 if __name__ == '__main__':
-    for item in get_physical_books(get_books_from_library('программирование', True)):
-        print(item)
+    content = open('test.html').read()
+    print(get_physical_books(content))
