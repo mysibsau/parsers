017e2b8beae732ce8a4ba79af2fdbfdc3c958486
kiri11_mi1 <kirill.mil.0310@mail.ru>
2021-03-26 09:43:00 +0700

    заголовки в запрос для получения книгохранения

--- library_parser/getters.py
+++ library_parser/getters.py
@@ -45,7 +45,6 @@ def get_books_from_library(key_words: str, physical: bool = True) -> str:
             'reqs_outputed': 0,
             'last_output_time': 0,
             'finish_flag': 'last'
-
         }
     )
 
@@ -58,7 +57,18 @@ def delete_bo_lighting_tag(html: str) -> str:
 
 
 def get_book_holders(url_part: str) -> str:
-    response = requests.get(BASE_URL+url_part)
+    headers = {
+        'Accept': "*/*",
+        "Accept-Encoding": "gzip, deflate",
+        "Accept-Language": "ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7",
+        "Connection": "keep-alive",
+        "Cookie": "_ga=GA1.2.636315675.1608545000; f_search_mode=STEASY; JSESS3=7ecc3798b86b9b879eef0360f4e63a7b; cltid=1529; trcusr=155; e7b861d63c4aad58e88ad02684ae99d0=9c3ae27386f8e67fdda04ce675048c56; js_vsid=4917",
+        "Host": "biblioteka.sibsau.ru",
+        "Referer": "http://biblioteka.sibsau.ru/jirbis2/",
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.190 Safari/537.36",
+        "X-Requested-With": "XMLHttpRequest",
+    }
+    response = requests.get(url=BASE_URL+url_part, headers=headers)
     if response.status_code == 200:
         return response.text
 
@@ -71,5 +81,5 @@ def get_book_holders(url_part: str) -> str:
 
 
 if __name__ == '__main__':
-    print(get_books_from_library(key_words='Программирование', physical=True))
+    print(get_books_from_library(key_words='Ефремов', physical=True))
     # print(get_books_from_library('Программирование', False))
